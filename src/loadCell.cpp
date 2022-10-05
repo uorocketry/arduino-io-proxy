8d7d2ac92e9e6368a995f60d2d44d2e39abebcb0
Jonathan Papineau <dev@jontech.app>
2022-10-04 20:20:43 -0400

    set load cell pins

--- src/loadCell.cpp
+++ src/loadCell.cpp
@@ -4,15 +4,14 @@
 #include "pb_encode.h"
 #include <Arduino.h>
 
-constexpr uint8_t PDSCK = 0;
-constexpr uint8_t DOUT = 0;
+constexpr uint8_t PDSCK = A5;
+constexpr uint8_t DOUT = A4;
 
-// TODO: Change this
 #define PORT_CLOCK PORTB
-#define BIT_CLOCK B00000100
+#define BIT_CLOCK B00100000
 
 #define PIN_OUT PINB
-#define BIT_OUT B00000001
+#define BIT_OUT B00010000
 
 // Single clock cycle delay
 #define NOP __asm__ __volatile__("nop\n\t")
